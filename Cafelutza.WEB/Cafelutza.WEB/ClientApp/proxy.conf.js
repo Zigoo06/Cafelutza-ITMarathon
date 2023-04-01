5a7b8a16333afc0010d87690ce6ff9a879750bd5
Laurentiu <baltalaur@gmail.com>
2023-04-02 00:46:18 +0300

    added proxy

---
+++ Cafelutza.WEB/Cafelutza.WEB/ClientApp/proxy.conf.js
@@ -0,0 +1,17 @@
+const { env } = require("process");
+
+const target = "https://localhost:7221";
+
+const PROXY_CONFIG = [{
+  context: [
+    "/api",
+  ],
+  target: target,
+  secure: false,
+  headers: {
+    Connection: "Keep-Alive",
+  },
+  }
+];
+
+module.exports = PROXY_CONFIG;
--- Cafelutza.WEB/Cafelutza.WEB/Program.cs
+++ Cafelutza.WEB/Cafelutza.WEB/Program.cs
@@ -1,10 +1,15 @@
 using Cafelutza.Application.Services;
 using Cafelutza.Data;
 using Cafelutza.Data.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddSpaStaticFiles(options =>
+{
+    options.RootPath = "../Cafelutza.SPA";
+});
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IRaportRepository, RaportRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
@@ -13,7 +18,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DatabaseContext>(x=> x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddAuthentication(GoogleDefaults.AuthenticationScheme).
+    AddCookie().
+    AddGoogle(options =>
+{
+    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.CallbackPath = "/sign-in";
+    options.ClientId = builder.Configuration["Google:ClientId"];
+    options.ClientSecret = builder.Configuration["Google:ClientSecret"];
+    options.ClaimsIssuer = "Google";
+    //options.Scope.Add("userinfo.email");
+    //options.Scope.Add("userinfo.profile");
+});
+builder.Services.AddAuthorization();
 
 builder.Services.AddCors(policy =>
 {
@@ -41,6 +58,7 @@ using (var scope = app.Services.CreateScope())
 
 app.UseHttpsRedirection();
 app.UseCors("DefaultPolicy");
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
