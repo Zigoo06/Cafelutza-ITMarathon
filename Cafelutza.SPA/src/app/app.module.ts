ed3997b8069a0a5b9542fc1f2fc9b70fc0447e65
cucu alex <cucualexandru15@yahoo.com>
2023-04-01 15:36:31 +0300

    asdasd

--- Cafelutza.SPA/src/app/app.module.ts
+++ Cafelutza.SPA/src/app/app.module.ts
@@ -17,9 +17,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-
+import { AnalyzePageComponent } from './analyze-page/analyze-page.component';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, ReportsComponent],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    ReportsComponent,
+    AnalyzePageComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -34,6 +40,7 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
