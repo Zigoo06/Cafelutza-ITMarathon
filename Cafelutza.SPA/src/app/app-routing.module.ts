17314e98b91a8f61d48edeb97bbfda4b184f6417
Gitea <eusebiu_ureke@yahoo.com>
2023-04-01 17:03:05 +0300

    dadada

--- Cafelutza.SPA/src/app/app-routing.module.ts
+++ Cafelutza.SPA/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 import { ReportsComponent } from './reports/reports.component';
 
-const routes: Routes = [{ path: 'reports', component: ReportsComponent }];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'reports', component: ReportsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
