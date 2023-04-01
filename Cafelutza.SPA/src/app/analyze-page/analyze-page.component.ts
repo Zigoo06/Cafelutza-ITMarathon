6ea814803cb57e41529917b590ae191429ca1f6a
cucu alex <cucualexandru15@yahoo.com>
2023-04-02 00:20:31 +0300

    merge

--- Cafelutza.SPA/src/app/analyze-page/analyze-page.component.ts
+++ Cafelutza.SPA/src/app/analyze-page/analyze-page.component.ts
@@ -2,6 +2,27 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Raport } from '../models/raport';
 import { RaportService } from '../services/raport.service';
 
+// @Component({
+//   selector: 'app-analyze-page',
+//   templateUrl: './analyze-page.component.html',
+//   styleUrls: ['./analyze-page.component.scss'],
+// })
+// export class AnalyzePageComponent implements OnInit {
+//   selectedItem = '';
+
+//   listItems: Raport[] = [];
+//   constructor(private raportService: RaportService) {}
+
+//   ngOnInit(): void {
+//     this.getData();
+//   }
+//   getData() {
+//     this.raportService.getRaports().subscribe((response) => {
+//       this.listItems = response;
+//     });
+//     console.log(this.listItems);
+//   }
+// }
 @Component({
   selector: 'app-analyze-page',
   templateUrl: './analyze-page.component.html',
@@ -19,7 +40,7 @@ export class AnalyzePageComponent implements OnInit {
   getData() {
     this.raportService.getRaports().subscribe((response) => {
       this.listItems = response;
+      console.log(this.listItems);
     });
-    console.log(this.listItems);
   }
 }
