089dfb0cdf3537daaea1ae3241d88eb3f6839adf
Gitea <eusebiu_ureke@yahoo.com>
2023-04-01 15:27:56 +0300

    dada

--- Cafelutza.SPA/src/app/app.module.ts
+++ Cafelutza.SPA/src/app/app.module.ts
@@ -17,9 +17,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, ReportsComponent],
+  declarations: [AppComponent, NavBarComponent, ReportsComponent, HomePageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
