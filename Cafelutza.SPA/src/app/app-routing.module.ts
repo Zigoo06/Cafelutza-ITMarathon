0dc6296dd1f6fd7a907d06992bbe1625569547ae
Gitea <eusebiu_ureke@yahoo.com>
2023-04-01 15:32:20 +0300

    merge

--- Cafelutza.SPA/src/app/app-routing.module.ts
+++ Cafelutza.SPA/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReportsComponent } from './reports/reports.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'reports', component: ReportsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
