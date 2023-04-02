f8275378cb4a784f73a82ec9f5f9feee1a928921
Gitea <eusebiu_ureke@yahoo.com>
2023-04-02 03:31:46 +0300

    comitere

--- Cafelutza.WEB/Cafelutza.WEB/ClientApp/src/app/analyze-page/analyze-page.component.ts
+++ Cafelutza.WEB/Cafelutza.WEB/ClientApp/src/app/analyze-page/analyze-page.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Raport } from '../models/raport';
 import { RaportService } from '../services/raport.service';
+import { map } from 'rxjs/operators';
 
 // @Component({
 //   selector: 'app-analyze-page',
@@ -35,12 +36,24 @@ export class AnalyzePageComponent implements OnInit {
   constructor(private raportService: RaportService) {}
 
   ngOnInit(): void {
-    this.getData();
-  }
-  getData() {
-    this.raportService.getRaports().subscribe((response) => {
-      this.listItems = response;
-      console.log(this.listItems);
-    });
+    //this.getData(){
+    this.raportService
+      .getRaports()
+      .pipe(
+        map((response: { [key: number]: Raport }) => {
+          const raportul = [];
+          for (const key in response) {
+            if (response.hasOwnProperty(key)) {
+              raportul.push({ ...response[key], id: key });
+            }
+            raportul.push({ ...response[key], id: key });
+          }
+          return raportul;
+        })
+      )
+      .subscribe((response) => {
+        this.listItems = response;
+        console.log(this.listItems);
+      });
   }
 }
